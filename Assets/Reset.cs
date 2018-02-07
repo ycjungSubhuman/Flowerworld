f7764c924567007673ab9e8bfd600b5648a99261
P17seongbin <33628727+P17seongbin@users.noreply.github.com>
2018-02-07 10:58:17 +0900

    Add Reset(Press R for 2 secs) and BacktoMain(Press ESC for 2 secs) Func

---
+++ Assets/Reset.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Reset : MonoBehaviour
+{
+
+    public bool Pressed = false;
+    private bool Hovered = false;
+
+    public bool Is_Reset;
+    public bool Is_BacktoMain;
+
+    Slider ResetProgress;
+
+    public GameObject Player;
+
+    float SLIDER_DELTA = 0.5f;//초당 0.5
+
+    // Use this for initialization
+    void Start()
+    {
+        ResetProgress = transform.Find("ProgressBar").GetComponent<Slider>();
+        SLIDER_DELTA = 0.5f;
+       // StartCoroutine(DetectMouseHovering());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player == null)
+            Player = GameObject.FindWithTag("Player");
+        if (Pressed || Hovered)
+        {
+            ResetProgress.value += SLIDER_DELTA * Time.deltaTime;
+            if (ResetProgress.value > 1f)
+                ResetProgress.value = 1f;
+        }
+        else
+        {
+            ResetProgress.value -= SLIDER_DELTA * Time.deltaTime;
+            if (ResetProgress.value < 0)
+                ResetProgress.value = 0;
+        }
+        if (ResetProgress.value >= 1f)
+        {
+            if (Is_Reset)
+                Player.GetComponent<PlayerControlScript>().onResetKey();
+            if (Is_BacktoMain)
+                Player.GetComponent<PlayerControlScript>().onGotoStageSelect();
+            ResetProgress.value = 0;
+        }
+
+
+    }
+    private void OnMouseOver()
+    {
+        Hovered = true;
+    }
+    private void OnMouseExit()
+    {
+        Hovered = false;
+    }
+    //호버링 감지 방식 변경중(Legacy)
+    /*
+    IEnumerator DetectMouseHovering()
+    {
+        while (true)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if (hit.collider.gameObject == this.gameObject)
+                    Hovered = true;
+                else
+                    Hovered = false;
+            }
+            yield return new WaitForSeconds(0.2f);
+        }
+    }*/
+}
