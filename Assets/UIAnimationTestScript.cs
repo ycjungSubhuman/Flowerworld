a9d558173984326a5a45f252e000b393e8c51cd2
SungBinPark <psb2942@gmail.com>
2018-02-18 14:27:51 +0900

    Implement UIButton Animations & Controller

---
+++ Assets/UIAnimationTestScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIAnimationTestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void Update() {
+
+    }
+
+    // Update is called once per frame
+    private void OnMouseEnter() {
+        GetComponent<Animator>().SetTrigger( "Shake" );
+    }
+    private void OnMouseExit() {
+        GetComponent<Animator>().SetTrigger( "Out" );
+    }
+    private void OnMouseOver() {
+        if( Input.GetMouseButtonDown( 0 ) ) {
+            GetComponent<Animator>().SetTrigger( "Vibrate" );
+        } else if( Input.GetMouseButtonDown( 1 ) ) {
+            GetComponent<Animator>().SetTrigger( "Stomp" );
+        }
+    }
+}
