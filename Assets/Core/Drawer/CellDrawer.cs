35a80a6fb57afa8e7b14bae95b9b79e2dcff4bad
SungBinPark <psb2942@gmail.com>
2018-02-20 18:27:45 +0900

    Cell 이미지 버전 제작(Legacy 버전 남겨둠)

--- Assets/Core/Drawer/CellDrawer.cs
+++ Assets/Core/Drawer/CellDrawer.cs
@@ -9,25 +9,62 @@ namespace Assets.Core.Drawer
 {
     class CellDrawer : ICellDrawer
     {
+        public bool ImageCell = true;
         public GameObject Draw(Cell cell)
         {
-            var cellPrefab = Resources.Load<GameObject>("prefabs/Cell");
-            var cellGameObject = GameObject.Instantiate (cellPrefab);
-            var material = cellGameObject.GetComponent<MeshRenderer> ().material;
-            material.SetColor ("_LineColor", CellColor.getLineColorOf (cell.label));
-            material.SetColor ("_BackgroundColor", CellColor.getBackgroundColorOf (cell.label));
-            material.SetFloat ("_Offset", UnityEngine.Random.value);
+            GameObject cellPrefab;
+            GameObject cellGameObject;
+            Material material;
+            Sprite sprite;
+            if( !ImageCell ) {
+                cellPrefab = Resources.Load<GameObject>( "prefabs/Cell" );
+                cellGameObject = GameObject.Instantiate( cellPrefab );
+                material = cellGameObject.GetComponent<MeshRenderer>().material;
+                material.SetColor( "_LineColor", CellColor.getLineColorOf( cell.label ) );
+                material.SetColor( "_BackgroundColor", CellColor.getBackgroundColorOf( cell.label ) );
+                material.SetFloat( "_Offset", UnityEngine.Random.value );
 
-            // Add Goal Mark to Goal Cell
-            if(cell.label.FallIn(Label.GOAL))
-            {
-                var goalMarker = GameObject.Instantiate (cellGameObject);
-                var goalMarkerMaterial = Resources.Load<Material> ("materials/BorderMaterial");
-                goalMarker.GetComponent<MeshRenderer> ().material = goalMarkerMaterial;
-                goalMarker.transform.parent = cellGameObject.transform;
+                // Add Goal Mark to Goal Cell
+                if( cell.label.FallIn( Label.GOAL ) ) {
+                    var goalMarker = GameObject.Instantiate( cellGameObject );
+                    var goalMarkerMaterial = Resources.Load<Material>( "materials/BorderMaterial" );
+                    goalMarker.GetComponent<MeshRenderer>().material = goalMarkerMaterial;
+                    goalMarker.transform.parent = cellGameObject.transform;
+                }
+            } else {
+                cellPrefab = Resources.Load<GameObject>( "prefabs/ImageCell" );
+                cellGameObject = GameObject.Instantiate( cellPrefab );
+                //Cell의 label에 따라 다른 이미지를 가져온다
+                sprite = FlowerFetcher( cell.label );
+                cellGameObject.GetComponent<SpriteRenderer>().sprite = sprite;
+
+                if( cell.label.FallIn( Label.GOAL ) ) {
+                    var goalMarker = GameObject.Instantiate( cellGameObject );
+                    var goalMarkerMaterial = Resources.Load<Material>( "materials/BorderMaterial" );
+                   // goalMarker.GetComponent<MeshRenderer>().material = goalMarkerMaterial;
+                   // goalMarker.transform.parent = cellGameObject.transform;
+                }
             }
-
             return cellGameObject;
+        }
+
+        Sprite FlowerFetcher( Label label ) {
+
+            string FlowerName;
+            if( label.Value == Label.A.Value ) {
+                FlowerName = "A";
+            } else if( label.Value == Label.B.Value ) {
+                FlowerName = "B";
+            } else if( label.Value == Label.C.Value ) {
+                FlowerName = "C";
+            } else if( label.Value == Label.D.Value ) {
+                FlowerName = "D";
+            } else {
+                FlowerName = "D";
+            }
+            Sprite temp = Resources.Load<Sprite>( "Sprite/CellFlower/" + FlowerName );
+
+            return temp;
         }
 
         public float Width()
