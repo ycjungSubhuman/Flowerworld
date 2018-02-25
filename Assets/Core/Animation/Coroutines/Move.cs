db7e9cfa2f31ece0f4610129fd4d63df443707a5
P17seongbin <bsbletter@gmail.com>
2018-02-25 21:23:41 +0900

    Add Menu Animations

--- Assets/Core/Animation/Coroutines/Move.cs
+++ Assets/Core/Animation/Coroutines/Move.cs
@@ -16,20 +16,32 @@ namespace Assets.Core.Animation.Coroutines
             return Interpolate (gameObject, start, goal, duration, tModifier);
         }
 
+        public static IEnumerator QuadOut(Action<Vector2> update, Vector2 start, Vector2 goal, float duration)
+        {
+            //움직임을 기술하는 함수(지금은 볼록한 2차함수)
+            Func<float, float> tModifier = t => -(t - 1) * (t - 1) + 1;
+            return Interpolate (update, start, goal, duration, tModifier);
+        }
+
         private static IEnumerator Interpolate(GameObject gameObject, Vector2 start, Vector2 goal, float duration, Func<float, float> tModifier)
+        {
+            return Interpolate ((v) => { gameObject.transform.localPosition = v; }, start, goal, duration, tModifier);
+        }
+
+        private static IEnumerator Interpolate(Action<Vector2> update, Vector2 start, Vector2 goal, float duration, Func<float, float> tModifier)
         {
             float t = 0;
             float time = 0;
 
-            while(time <= duration)
+            while ( time <= duration )
             {
                 time += Time.deltaTime;
                 t = time / duration;
-                gameObject.transform.localPosition = Vector2.Lerp (start, goal, tModifier(t));
+                update(Vector2.Lerp (start, goal, tModifier (t)));
                 yield return null;
             }
 
-            gameObject.transform.localPosition = goal;
+            update(goal);
         }
     }
 }
