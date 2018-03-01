41bc119f25ad33365f61cf76f0e67de8f11290e1
ycjungSubhuman <jung3519@gmail.com>
2018-03-01 17:04:53 +0900

    Add Exit Button

---
+++ Assets/GetOut.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetOut : MonoBehaviour {
+
+    public void onGetOut()
+    {
+        Application.Quit ();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            onGetOut ();
+        }
+    }
+}
